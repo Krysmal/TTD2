ffc187af41e90d1212c76596eb9a0d8769669c9f
Krzysztof Malinowski <malinowskizxcvbnm@gmail.com>
2025-04-22 13:22:41 +0200

    program done

--- TTD2/Program.cs
+++ TTD2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;  
+public class ShoppingCart { private List<Item> items; 
+    public ShoppingCart() { items = new List<Item>(); } 
+    public void AddItem(string name, double price, int quantity) 
+    {
+        if (price < 0 || quantity <= 0)         
+        { throw new ArgumentException("Price and quantity must be positive."); 
+        } 
+        items.Add(new Item(name, price, quantity)); 
+    } 
+    public void RemoveItem(string name) 
+    {   
+        var item = items.FirstOrDefault(i => i.Name == name); 
+        if (item == null) 
+        { 
+            throw new ArgumentException("Item not found in the cart."); 
+        } 
+        items.Remove(item); 
+    } 
+    public double GetTotalPrice() 
+    { 
+        return items.Sum(i => i.Price * i.Quantity); 
+    } 
+    public void ClearCart() { items.Clear(); 
+    } 
+    public List<Item> GetItems() { return items; } 
+    }
+public class Item { public string Name { get; } public double Price { get; } public int Quantity { get; }
+public Item(string name, double price, int quantity) { Name = name; Price = price; Quantity = quantity; } } 
